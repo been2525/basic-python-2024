07e2b14c54191390b2920e38d82490a50bb21a2c
been2525 <qkrtjdqls245@naver.com>
2024-02-05 16:58:57 +0900

    쓰레드 학습!

---
+++ day06/test38_pyqt.py
@@ -0,0 +1,38 @@
+# file : test38_pyqt.py
+# desc : Qt디자이너 만든 ui와 연동
+
+import sys
+from PyQt5 import   QtGui, QtWidgets, uic
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+class qtwin_exam(QWidget):
+    def __init__(self) -> None:
+        super().__init__()
+        uic.loadUi('./day06/TestApp.ui', self) # QtDesigner에서 만든 ui를 로드
+        # 버튼에 대한 시그널처리
+        self.btnStart.clicked.connect(self.btnStartCliked) # ui 파일내에 있는 위젯접근은 VSCode상에서 색상으로 표시x
+        self.btnStop.clicked.connect(self.btnStopCliked)
+
+    def btnStartCliked(self):
+        print('시작버튼 클릭')
+        self.lblStatus.setText('상태 : 동작시작')
+        QMessageBox.about(self, '동작', '***시스템이 시작되었습니다')
+
+    def btnStopCliked(self):
+        print('종료버튼 클릭')
+        self.lblStatus.setText('상태 : 동작중지')
+
+    def closeEvent(self, QCloseEvent) -> None: # x버튼 종료확인
+        re = QMessageBox.question(self, '종료 확인', '종료할래?', QMessageBox.Yes|QMessageBox.No)
+        if re == QMessageBox.Yes: # 닫기
+            QCloseEvent.accept()
+        else:
+            QCloseEvent.ignore()
+
+if __name__ == '__main__':
+    loop = QApplication(sys.argv)
+    instance = qtwin_exam()
+    instance.show()
+    loop.exec_()
---
+++ day06/test39_nothread.py
@@ -0,0 +1,41 @@
+# file : test39_nothread.py
+# desc : Qt에서 스레드 없이 동작테스트
+
+import sys
+from PyQt5 import   QtGui, QtWidgets, uic
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+class qtwin_exam(QWidget):
+    def __init__(self) -> None:
+        super().__init__()
+        uic.loadUi('./day06/ThreadApp.ui', self) # QtDesigner에서 만든 ui를 로드
+        # 버튼에 대한 시그널처리
+        self.btnStart.clicked.connect(self.btnStartCliked) # ui 파일내에 있는 위젯접근은 VSCode상에서 색상으로 표시x
+    
+    def btnStartCliked(self):
+        maxVal = 10000001
+        print('시작버튼 클릭')
+        self.txbLog.append('상태 : 동작시작')
+        self.pgbTask.setValue(0) #프로그레스바 0부터 시작
+        self.pgbTask.setRange(0, maxVal-1) # 0부터 100까지
+        for i in range(maxVal): # 0 ~ 100까지
+            print_str = f'노쓰레드 출력 >> {i}'
+            print(print_str)
+            self.txbLog.append(print_str)
+            self.pgbTask.setValue(i)
+
+        
+    def closeEvent(self, QCloseEvent) -> None: # x버튼 종료확인
+        re = QMessageBox.question(self, '종료 확인', '종료할래?', QMessageBox.Yes|QMessageBox.No)
+        if re == QMessageBox.Yes: # 닫기
+            QCloseEvent.accept()
+        else:
+            QCloseEvent.ignore()
+
+if __name__ == '__main__':
+    loop = QApplication(sys.argv)
+    instance = qtwin_exam()
+    instance.show()
+    loop.exec_()
